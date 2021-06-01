c73209e7723d76e74e914617cb52878a3ca13447
WeichselRiver <34955350+WeichselRiver@users.noreply.github.com>
2021-06-01 09:19:06 +0200

    Create fastapi.py

---
+++ fastapi.py
@@ -0,0 +1,43 @@
+# pip install fastapi, uvicorn
+
+
+
+# minimal ---------------------------
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_hdl():
+  return {"message" : "Hallo"}
+
+
+# templates -------------------------
+
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name = "static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def read_hdl(request : Request):
+    """
+    Show entry page UploadHDL.html
+    """
+    return templates.TemplateResponse("UploadHDL.html", {"request": request})
+  
+  
+ #   upload files (Excel) --------------------------------------
+  
+ ...
+
+@app.post("/")
+def create_upload_file(file: bytes = File(...)):
+      
+    snapshot_file = BytesIO(file).read() # read file and convert to pandas dataframe
+    workbook = pd.ExcelFile(snapshot_file)
